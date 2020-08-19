51d1e95f64c82e789ab545ff42e61280c259da64
din <dinndev@gmail.com>
2020-08-19 15:09:56 +0800

    Converting kelvin to Fahrenheit

---
+++ Projects/Kelvin.js
@@ -0,0 +1,14 @@
+
+// 293 kelvin forecast 
+const kelvin = 124234;
+
+// kelvin to celcius
+const celcius = kelvin - 273
+
+// calculating the celcius to get the fahrenheit using its format
+const fahrenheit =  Math.floor(celcius * (9/5) + 32);
+
+
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
