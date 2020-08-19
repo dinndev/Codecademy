256150111b3b896d3fcf8c345d24c0a62781ee6e
din <dinndev@gmail.com>
2020-08-19 15:27:51 +0800

    Converting Human age into Dog years

---
+++ Projects/Dog-days.js
@@ -0,0 +1,7 @@
+// My age so fresh! someone kiss me 
+const myAge = 20;
+const myName = 'Din'
+
+const myAgeInDogYears = (2 * 10.5) + ((myAge - 2) * 4);
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
